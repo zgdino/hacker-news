4189e6f153a47458bec55edfb66088d8a8daf6ae
Dino Hadzic <zgdino@yahoo.com>
2022-05-26 09:57:45 +0200

    setting up the reducer

--- setup/src/reducer.js
+++ setup/src/reducer.js
@@ -6,5 +6,14 @@ import {
   HANDLE_SEARCH,
 } from './actions'
 
-const reducer = () => {}
+const reducer = (state, action) => {
+  // using switch instead of if statements
+  switch (action.type) {
+    case 'SET_LOADING':
+      return { ...state, isLoading: true }
+      // if none of the actions passed match
+    default:
+      throw new Error (`no matching "${action.type}" action type`)
+  }
+}
 export default reducer

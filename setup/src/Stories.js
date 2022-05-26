eb1f946fadeb1144e4a6660e21d92df9591441fa
Dino Hadzic <zgdino@yahoo.com>
2022-05-26 10:27:14 +0200

    comments

--- setup/src/Stories.js
+++ setup/src/Stories.js
@@ -4,7 +4,7 @@ import { useGlobalContext } from './context'
 
 const Stories = () => {
   const { isLoading } = useGlobalContext()
-
+  // in case of loading being true display spinner; search for class loading in css file
   if (isLoading) {
     return <div className='loading'></div>
   }

f720e57e49d7bec10d085d34917aa3bc49928936
Dino Hadzic <zgdino@yahoo.com>
2022-05-27 17:27:38 +0200

    stories setup

--- setup/src/Stories.js
+++ setup/src/Stories.js
@@ -3,13 +3,20 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext()
+  const { isLoading, hits } = useGlobalContext()
   // in case of loading being true display spinner; search for class loading in css file
   if (isLoading) {
     return <div className='loading'></div>
   }
 
-  return <h2>stories component</h2>
+  return <section className="stories">
+    {hits.map((story) => {
+      console.log(story);
+      <article className="story">
+        single story
+      </article>
+    })}
+  </section>
 }
 
 export default Stories

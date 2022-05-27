c71fb6afc80ddae12cb6d24222bc02c00b5d3f6c
Dino Hadzic <zgdino@yahoo.com>
2022-05-27 18:18:44 +0200

    layout of each story established

--- setup/src/Stories.js
+++ setup/src/Stories.js
@@ -8,15 +8,29 @@ const Stories = () => {
   if (isLoading) {
     return <div className='loading'></div>
   }
-  console.log(hits)
   return (
     <section className='stories'>
       {hits.map((story) => {
         const { objectID, title, num_comments, url, points, author } = story
-        return <article key={objectID} className='story'>
-          <h4 className="title">{title}</h4>
-          <p className="info">{points} points by <span>{author} |</span> {num_comments}{' '} comments</p>
-        </article>
+        return (
+          <article key={objectID} className='story'>
+            <h4 className='title'>{title}</h4>
+            <p className='info'>
+              {points} points by <span>{author} |</span> {num_comments} comments
+            </p>
+            <div>
+              <a
+                href={url}
+                className='read-link'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                read more
+              </a>
+              <button className='remove-btn'>remove</button>
+            </div>
+          </article>
+        )
       })}
     </section>
   )

c6a341fec2a6c37ed78fe1f414980df684694d72
Dino Hadzic <zgdino@yahoo.com>
2022-05-29 02:36:55 +1000

    handle search in reducer

--- setup/src/reducer.js
+++ setup/src/reducer.js
@@ -22,12 +22,14 @@ const reducer = (state, action) => {
       return {
         ...state,
         // return all the stories that are not matching removed story
-        hits: state.hits.filter((story) => story.objectID !== action.payload), 
+        hits: state.hits.filter((story) => story.objectID !== action.payload),
       }
     case HANDLE_SEARCH:
       return {
         ...state,
-        
+        query: action.payload,
+        // every time something is typed in a search bar change the page to 0, because if we are currently on page number 6 and start searching for a new term, we would like to start from page 0
+        page: 0,
       }
     // if none of the actions passed match
     default:

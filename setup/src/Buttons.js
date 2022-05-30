b0605c79392e91bc8c5dd1208ae371e8a4207fc7
Dino Hadzic <zgdino@yahoo.com>
2022-05-30 16:28:06 +0200

    prev button setup

--- setup/src/Buttons.js
+++ setup/src/Buttons.js
@@ -2,7 +2,14 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Buttons = () => {
-  return <h2>button container</h2>
+  const {isLoading, page, nbPages, hnadlePage} = useGlobalContext()
+  return <div className="btn-container">
+    {/* button is shortly disabled and cursor changes(look index.css) every time loading is true */}
+    <button disabled={isLoading} onClick={() => hnadlePage('dec')}>
+      prev
+    </button>
+    <p></p>
+  </div>
 }
 
 export default Buttons

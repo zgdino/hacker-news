abc31bc3474ba1321e7c1bc6f0e715d46dee3fb8
Dino Hadzic <zgdino@yahoo.com>
2022-05-30 16:34:01 +0200

    page number information

--- setup/src/Buttons.js
+++ setup/src/Buttons.js
@@ -2,14 +2,19 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Buttons = () => {
-  const {isLoading, page, nbPages, hnadlePage} = useGlobalContext()
-  return <div className="btn-container">
-    {/* button is shortly disabled and cursor changes(look index.css) every time loading is true */}
-    <button disabled={isLoading} onClick={() => hnadlePage('dec')}>
-      prev
-    </button>
-    <p></p>
-  </div>
+  const {isLoading, page, nbPages, handlePage} = useGlobalContext()
+  return (
+    <div className='btn-container'>
+      {/* button is shortly disabled and cursor changes(look index.css) every time loading is true */}
+      <button disabled={isLoading} onClick={() => handlePage('dec')}>
+        prev
+      </button>
+      <p>{page + 1} of {nbPages}</p>
+      <button disabled={isLoading} onClick={() => handlePage('inc')}>
+        next
+      </button>
+    </div>
+  )
 }
 
 export default Buttons
